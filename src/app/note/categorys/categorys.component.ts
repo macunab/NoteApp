1130bf65f61c47f57217eefff9ef7b143aafd2ea
Marshall <mn.acunab@gmail.com>
2022-08-27 19:08:39 -0300

    categories template

--- src/app/note/categorys/categorys.component.ts
+++ src/app/note/categorys/categorys.component.ts
@@ -4,6 +4,26 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-categorys',
   templateUrl: './categorys.component.html',
   styles: [
+    `
+    .content {
+      margin-top: 2em;
+    }
+
+    .dot {
+      height: 15px;
+      width: 15px;
+      border-radius: 50%;
+      display: inline-block;
+      margin-right: 10px;
+    }
+
+    @media only screen and (min-width: 1000px) {
+    .content {
+        margin-right: 150px;
+        margin-left: 150px;
+    }
+}
+    `
   ]
 })
 export class CategorysComponent implements OnInit {
--- src/app/note/note-routing.module.ts
+++ src/app/note/note-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategorysComponent } from './categorys/categorys.component';
 import { CreateNoteComponent } from './create-note/create-note.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     children: [
       { path: 'all', component: ListComponent },
       { path: 'create', component: CreateNoteComponent },
+      { path: 'categories', component: CategorysComponent },
       { path: '**', redirectTo: 'all' }
     ]
    },
