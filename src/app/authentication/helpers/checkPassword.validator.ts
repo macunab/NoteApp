e9db77ee0586c373b791ec0b227d645d52a25e52
Marshall <mn.acunab@gmail.com>
2022-09-18 11:34:43 -0300

    confirm password validator

---
+++ src/app/authentication/helpers/checkPassword.validator.ts
@@ -0,0 +1,16 @@
+import { FormGroup } from "@angular/forms";
+
+export function CheckPasswordValidator(controlName: string, matchingControlName: string) {
+    return ( formGroup: FormGroup ) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+        if(matchingControl.errors && !matchingControl.errors['checkPasswordValidator']) {
+            return;
+        }
+        if(control.value !== matchingControl.value) {
+            matchingControl.setErrors({ checkPasswordValidator: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
--- src/app/note/user-configuration/user-configuration.component.ts
+++ src/app/note/user-configuration/user-configuration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CheckPasswordValidator } from 'src/app/authentication/helpers/checkPassword.validator';
 
 @Component({
   selector: 'app-user-configuration',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserConfigurationComponent implements OnInit {
 
-  constructor() { }
+  changePasswordForm: FormGroup = this.fb.group({
+    password: ['', [ Validators.required ]],
+    confirmPassword: ['', [ Validators.required ]]
+  },
+  {
+    validator: CheckPasswordValidator('password', 'confirmPassword')
+  });
+  
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  updatePassword() {
+
   }
 
 }
